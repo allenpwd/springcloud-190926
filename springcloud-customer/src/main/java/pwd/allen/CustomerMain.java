f4d43bb3953f60c7538edc54c4374e6f7dd28cf7
门那粒沙 <994266136@qq.com>
2020-03-29 20:39:36 +0800

    笔记补充

--- springcloud-customer/src/main/java/pwd/allen/CustomerMain.java
+++ springcloud-customer/src/main/java/pwd/allen/CustomerMain.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * @EnableDiscoveryClient基于spring-cloud-commons, @EnableEurekaClient基于spring-cloud-netflix
+ *  如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient
+ *
  * @author 门那粒沙
  * @create 2019-09-26 22:43
  **/
