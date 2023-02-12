d8ab5f78707a324fcf90bb503219e1d3522346db
门那粒沙 <994266136@qq.com>
2023-02-12 19:26:38 +0800

    错别字纠正

--- springcloud-provider/src/test/java/RxjavaTest.java
+++ springcloud-provider/src/test/java/RxjavaTest.java
@@ -15,7 +15,7 @@ public class RxjavaTest {
      * 能够发射0或n个数据，并以成功或者错误事件终止
      *  Observable：被观察者，决定什么时候触发事件以及触发怎样的事件
      *  Observer：观察者，可以在未来的某个时刻响应Observable的通知而不需要阻塞等待Observable发射数据
-     *  subscribe：订阅，建创建的Observable和Observer连接起来，只有使用了subscribe被观察者才会开始发送数据
+     *  subscribe：订阅，将创建的Observable和Observer连接起来，只有使用了subscribe被观察者才会开始发送数据
      */
     @Test
     public void Observable() {
@@ -33,7 +33,7 @@ public class RxjavaTest {
         });
         //将传递的数据类型进行转换
         Observable<Integer> observable4Int = observable4Str.map(s -> Integer.parseInt(s));
-        //调用subscribe后阻塞知道数据流完成
+        //调用subscribe后阻塞直到数据流完成
         observable4Int.subscribe(new Observer<Integer>() {
             @Override
             public void onCompleted() {
